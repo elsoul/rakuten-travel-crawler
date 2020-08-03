34049ee4fd946e5f68cdffeeaf8590f2a8e6b48a
FUMI-POPPIN <fumitake.kawasaki@el-soul.com>
2020-08-02 22:27:34 -0700

    first commit

---
+++ lib/rakuten/travel/crawler.rb
@@ -0,0 +1,10 @@
+require "rakuten/travel/crawler/version"
+
+module Rakuten
+  module Travel
+    module Crawler
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ spec/rakuten/travel/crawler_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Rakuten::Travel::Crawler do
+  it "has a version number" do
+    expect(Rakuten::Travel::Crawler::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end

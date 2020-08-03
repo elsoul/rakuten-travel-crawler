59ddc18b2fe89262950f32b9ee5344fabe9f5ba8
FUMI-POPPIN <fumitake.kawasaki@el-soul.com>
2020-08-03 12:02:13 -0700

    add console

--- lib/rakuten/travel/crawler.rb
+++ lib/rakuten/travel/crawler.rb
@@ -9,7 +9,23 @@ module Rakuten
   module Travel
     module Crawler
       class Error < StandardError; end
-      # Your code goes here...
+
+      def self.get_selenium_driver mode: :chrome
+        case mode
+        when :firefox_remote_capabilities
+          firefox_capabilities = Selenium::WebDriver::Remote::Capabilities.firefox
+          Selenium::WebDriver.for(:remote, url: "http://hub:4444/wd/hub", desired_capabilities: firefox_capabilities)
+        when :firefox
+          Selenium::WebDriver.for :firefox
+        else
+          options = Selenium::WebDriver::Chrome::Options.new
+          options.add_argument("--ignore-certificate-errors")
+          options.add_argument("--disable-popup-blocking")
+          options.add_argument("--disable-translate")
+          options.add_argument("-headless")
+          Selenium::WebDriver.for :chrome, options: options
+        end
+      end
     end
   end
 end
